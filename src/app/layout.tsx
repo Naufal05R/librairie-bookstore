d643dc54954469d19859860590f503355d7a4809
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-03-05 06:27:01 +0700

    feat(layout): update font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans, Work_Sans } from "next/font/google";
+import { ThemeProvider } from "@/components/provider/theme-provider";
 import "./globals.css";
 
 const font_display = Plus_Jakarta_Sans({
   subsets: ["latin"],
   variable: "--font-display",
 });
-const font_body = Work_Sans({ subsets: ["latin"], variable: "--font-body" });
+
+const font_body = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-body",
+});
 
 export const metadata: Metadata = {
   title: "Librairie",
@@ -15,13 +20,15 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${font_display.className} ${font_body.className}`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${font_display.variable} ${font_body.variable} bg-white dark:bg-gray-900 min-h-screen`}
+      >
+        <ThemeProvider>{children}</ThemeProvider>
       </body>
     </html>
   );
