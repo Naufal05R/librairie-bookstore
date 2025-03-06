97392b0a3d95a7d7632a626dd50b9b57a81e681a
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2025-03-06 23:04:23 +0700

    feat(navigation): create a new separate navigations constant

---
+++ src/constants/navigations.ts
@@ -0,0 +1,6 @@
+export const NAVIGATIONS = [
+  { label: "Home", path: "/" },
+  { label: "Categories", path: "/categories" },
+  { label: "About", path: "/about" },
+  { label: "Contact", path: "/contact" },
+];
